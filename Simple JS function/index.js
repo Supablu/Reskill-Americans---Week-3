2eecfcfbd171ffe0708cfa600542781fc92d53c2
Luis del Valle-Lopez <82071559+Supablu@users.noreply.github.com>
2022-12-31 20:21:12 -0800

    Add files via git push
    uploading index.js and index.html for assignment

---
+++ Simple JS function/index.js
@@ -0,0 +1,12 @@
+let name = "Luis";
+let height = "6'2\"";
+let country = "United States";
+
+function myStats() {
+  console.log(name, height, country);
+  return alert(
+    `Your name: ${name} Your height: ${height} Your country: ${country}`
+  );
+}
+
+myStats();
